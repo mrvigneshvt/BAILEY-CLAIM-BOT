6e03f5d396c507c2989bf820d4fe2d5d86971f6e
VIXYZ <71164943+mrvigneshvt@users.noreply.github.com>
2023-12-19 15:25:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,106 @@
+import {DisconnectReason} from '@whiskeysockets/baileys';
+import baileyObject from '@whiskeysockets/baileys';
+import { useMultiFileAuthState } from '@whiskeysockets/baileys'
+
+import  Qrcode  from 'qrcode';
+import TelegramBot from 'node-telegram-bot-api';
+
+
+const botToken = '6552828975:AAF-XWJKsDm4PZ9ZNkgza9dcb1XCdq6B-Q8'
+const bot = new TelegramBot(botToken, { polling: true });
+
+async function connectionLogic(){
+    const {state,saveCreds} = await useMultiFileAuthState('auth_info_baileys');
+    const sock = baileyObject.makeWASocket({
+        printQRInTerminal: true,
+        auth: state,
+    })
+
+    sock.ev.on('connection-update', async(req,res)=>{
+        const {connection, lastDisconnect , qr} = update || {};
+
+
+        if(!qr){
+            /*const qrImageBuffer = await Qrcode.toBuffer(qr, { errorCorrectionLevel: 'H' });
+
+            if(qrImageBuffer){console.log(`buffer sent`)}
+
+            const targetUserId = '1767901454';
+        
+            await bot.sendPhoto(targetUserId, qrImageBuffer, { caption: 'Scan the QR code to log in' });
+
+            console.log(`qr sent to terminal`)*/
+            console.log(`qr is not coming`)
+            
+        }
+
+        if(connection === "close"){
+            const shouldReconnect = 
+            lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
+        }
+
+        if(shouldReconnect){
+            connectionLogic();
+        }
+    })
+
+    sock.ev.on('messages.update',(messageInfo)=>{
+        console.log(messageInfo)
+    })
+
+    
+
+    sock.ev.on('messages.upsert', async (vixyz) => {
+
+
+        const groupid = '120363186646230726@g.us'
+        const Dealer  = '917010892470@s.whatsapp.net'
+        const Dealer1  = '2348163376700@s.whatsapp.net'
+
+        const groupID1 = '120363132061932497@g.us'
+        const groupID2 = '120363181774801103@g.us'
+
+
+
+    if(vixyz.messages[0].key.participant === Dealer1){
+        //console.log(`message received from DEaeler`)
+        if (vixyz.messages[0].key.remoteJid === groupID1 || vixyz.messages[0].key.remoteJid === groupID2) {
+            const videoMessage = vixyz.messages[0].message.videoMessage;
+            const imageMessage = vixyz.messages[0].message.imageMessage;
+            const claimerGroup = vixyz.messages[0].key.remoteJid
+    
+            if (videoMessage && videoMessage.caption){
+                const caption = (videoMessage.caption || imageMessage.caption)
+                const captcha = caption.match(/Captcha: (\w+)/);
+    
+                if (captcha && captcha[1]) {
+                    const tier = await caption.match(/🪄 \*Tier:\* ([a-z A-Z 0-9]{1})/);
+                    if(tier[1] === 'S'){
+                    console.log(tier[1])
+                    setTimeout(()=>{
+                     sock.sendMessage(claimerGroup, { text: `#claim ${captcha[1]}` })
+                    },500)
+                    console.log(`message sent`);
+                     } }
+            }else if(imageMessage && imageMessage.caption){
+                const caption = imageMessage.caption
+                const captcha = caption.match(/Captcha: (\w+)/);
+    
+                if (captcha && captcha[1]) {
+                    const tier = await caption.match(/🪄 \*Tier:\* ([a-z A-Z 0-9]{1})/);
+
+                    if(tier[1] == 4 || tier[1] == 5 || tier[1] == 6 || tier[1] == 'S'){
+                    console.log(tier[1])
+                    setTimeout(()=>{
+                        sock.sendMessage(claimerGroup, { text: `#claim ${captcha[1]}` })
+                       },500)
+                    console.log(`message sent`)
+
+            }
+        }
+    }}}});
+    
+    sock.ev.on('creds.update', saveCreds)
+}
+
+connectionLogic()
